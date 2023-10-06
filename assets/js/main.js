f1f022cb96e4aeefe543e2b270cf8e70bb35284c
2thlesss <thomas.jadie.reeves@gmail.com>
2023-10-06 09:21:05 -0400

    updated invoke url

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,12 +103,12 @@ jQuery(document).ready(function($) {
 
     });
 
- // GET API REQUEST
+// GET API REQUEST
 async function get_visitors() {
     // call post api request function
     //await post_visitor();
     try {
-        let response = await fetch('https://z79uyas992.execute-api.us-east-1.amazonaws.com/default/VisitorCounter', {
+        let response = await fetch('https://sazrdfu551.execute-api.us-east-1.amazonaws.com/default/VisitorCounter', {
             method: 'GET',
         });
         let data = await response.json()
@@ -122,6 +122,7 @@ async function get_visitors() {
 
 
 get_visitors();
+
 
 
 });
